ed24b742ca80d88d757303595b4d5b984141287e
dorage-san <baloonflower554@gmail.com>
2024-02-24 15:57:08 +0900

    add plugin
    - discord presence

--- .config/nvim/lua/dorage/init.lua
+++ .config/nvim/lua/dorage/init.lua
@@ -25,8 +25,20 @@ Plug('https://github.com/folke/which-key.nvim') -- tooltips of keybindings
 Plug('https://github.com/nvimdev/dashboard-nvim') -- custom banner
 Plug('https://github.com/nvim-tree/nvim-web-devicons')
 Plug('https://github.com/mg979/vim-visual-multi') -- mulit cursor
+Plug('https://github.com/andweeb/presence.nvim') -- discord presence
 
 vim.call('plug#end')
+
+-- [default keymap]
+
+-- esc -> noh
+vim.keymap.set('n', '<esc>', function()
+	if vim.api.nvim_get_vvar('hlsearch') == 1 then
+			return ":nohl<CR><esc>"
+	end
+
+	return "<esc>"
+end, { silent = true, noremap = true})
 
 -- theme setup
 
@@ -262,3 +274,24 @@ local db = require('dashboard')
       },
     },
   })
+
+-- [discord presence setup]
+
+require("presence").setup({
+    -- General options
+    auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+    neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
+    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    enable_line_number  = false,                      -- Displays the current line number instead of the current project
+    show_time           = true,                       -- Show the timer
+
+    -- Rich Presence text options
+    editing_text        = "Working!",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  =  "Working!",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     =  "Working!",      -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text =  "Working!",       -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text        =  "Working!",             -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text      =  "Working!",           -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text    =  "Working!",       -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+})
