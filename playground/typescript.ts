b6d0dcaa7108abbb850a3bd86e40e022ce7b59af
dorage-san <baloonflower554@gmail.com>
2024-02-29 00:19:03 +0900

    -

---
+++ playground/typescript.ts
@@ -0,0 +1,30 @@
+
+
+function getTitle () {
+	let title = 10;
+
+	if(id > 10){
+		title  = 15;
+	} else if (id >20){
+		title = 20;
+	} else {
+		title = 30;
+	}
+
+	return title;
+
+	a = 2;
+}
+
+for(const i of [1,2,3]) {
+	console.log(i)
+}
+
+for(let i = 0; i < 2; i++){
+	console.log(2);
+}
+
+getTitle();
+
+let a = 2;
+let b = 3;
