1c265bcb31ee29bb93186946368d076db388aa66
dorage-san <baloonflower554@gmail.com>
2024-02-29 21:17:35 +0900

    add vim settings
    encoding settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,8 @@ vim.cmd([[ :set shiftwidth=2 ]])
 vim.cmd([[ :set smarttab ]])
 vim.cmd([[ :set softtabstop=2 ]])
 vim.cmd([[ :set mouse=a ]])
+vim.cmd([[ :set encoding=utf-8 ]])
+vim.cmd([[ :set fileencodings=utf-8,cp949 ]])
 
 vim.cmd([[ nnoremap <leader>p "+p ]])
 vim.cmd([[ vnoremap <leader>p "+p ]])
