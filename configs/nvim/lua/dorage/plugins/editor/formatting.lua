53bfdf4d73986b7f2ec1ba8a30b8c89d9d75c813
dorage-san <baloonflower554@gmail.com>
2024-03-21 23:36:40 +0900

    add lua formatting

--- configs/nvim/lua/dorage/plugins/editor/formatting.lua
+++ configs/nvim/lua/dorage/plugins/editor/formatting.lua
@@ -3,18 +3,24 @@ return {
 		"nvimtools/none-ls.nvim",
 	},
 	{
-		'MunifTanjim/prettier.nvim',
+		"MunifTanjim/prettier.nvim",
 		dependencies = {
 			"nvimtools/none-ls.nvim",
 		},
-		config = function ()
+		config = function()
 			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
 
 			local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 			local event = "BufWritePre" -- or "BufWritePost"
 			local async = event == "BufWritePost"
 
 			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.completion.spell,
+				},
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
 						vim.keymap.set("n", "<Leader>f", function()
@@ -44,7 +50,7 @@ return {
 			local prettier = require("prettier")
 
 			prettier.setup({
-				bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+				bin = "prettier", -- or `'prettierd'` (v0.23.3+)
 				filetypes = {
 					"css",
 					"graphql",
@@ -58,16 +64,17 @@ return {
 					"typescript",
 					"typescriptreact",
 					"yaml",
+					"astro",
 				},
 				cli_options = {
 					config_precedence = "prefer-file",
 				},
 			})
-		end
+		end,
 	},
 	{
-		'windwp/nvim-autopairs',
+		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
-	}
+	},
 }
--- configs/nvim/lua/dorage/plugins/lsp/snippet.lua
+++ configs/nvim/lua/dorage/plugins/lsp/snippet.lua
@@ -1,5 +1,12 @@
 return {
-	'l3mon4d3/luasnip',
-	lazy=true,
-	build = "make install_jsregexp"
+	{
+		'l3mon4d3/luasnip',
+		lazy=true,
+		build = "make install_jsregexp",
+		config = function()
+			s("trig", {
+				i(1), t"text", i(2), t"text again", i(3)
+			})
+		end
+	}
 }
