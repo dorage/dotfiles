bd1676befd6c92174efa2dac1ccca99171401f6a
dorage-san <baloonflower554@gmail.com>
2024-03-22 22:03:02 +0900

    update keymap
    - substitute

--- configs/nvim/lua/dorage/configs/shortcuts.lua
+++ configs/nvim/lua/dorage/configs/shortcuts.lua
@@ -1,5 +1,12 @@
-vim.keymap.set({'i', 'x', 'n', 's'}, '<leader>w', '<cmd>w<cr><esc>', {desc = "Save file"})
+-- blazing fast save
+vim.keymap.set({ "i", "x", "n", "s" }, "<leader>w", "<cmd>w<cr><esc>", { desc = "Save file" })
 
-vim.keymap.set({'n', 's'}, '<leader>y', '"+y', {desc = "Copy to clipboard"})
-vim.keymap.set({'n', 's'}, '<leader>p', '"+p', {desc = "Paste from clipboard"})
+-- yank to clipboard
+vim.keymap.set({ "n", "s" }, "<leader>y", '"+y', { desc = "Copy to clipboard" })
+-- paste from clipboard
+vim.keymap.set({ "n", "s" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set({ "n", "s" }, "<leader>P", '"+P', { desc = "Paste from clipboard" })
 
+-- better substitute
+-- references: https://www.youtube.com/watch?v=k_PBlhH-DKk&t=13s
+vim.keymap.set("n", "<leader>ss", ":%s/<C-R><C-W>/<C-R>0/g<CR>", { desc = "Substitute" })
