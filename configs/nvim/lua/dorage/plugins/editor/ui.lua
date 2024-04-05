d35a614558a63c329d00684f47c675fc0a5b49ff
dorage-san <baloonflower554@gmail.com>
2024-04-05 19:30:52 +0900

    resize git floaterm

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -108,7 +108,7 @@ return {
 	{
 		"https://github.com/voldikss/vim-floaterm",
 		config = function()
-			vim.keymap.set("n", "<leader>ag", "<Cmd>:FloatermNew --width=0.9 --height=0.9 lazygit<CR>")
+			vim.keymap.set("n", "<leader>ag", "<Cmd>:FloatermNew --width=0.95 --height=0.95 lazygit<CR>")
 		end,
 	},
 	-- show shortcut hints
