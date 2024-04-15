a1ae0cc83208f802077ca1471b33b685e3bda560
dorage-san <baloonflower554@gmail.com>
2024-04-15 22:27:44 +0900

    fix: setup plugins
    dressing
    notify

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -158,10 +158,26 @@ return {
 	-- pretty vim select/input hook
 	{
 		"stevearc/dressing.nvim",
-		opts = {},
+		lazy = true,
+		init = function()
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
 	},
 	-- notify
 	{
 		"rcarriga/nvim-notify",
+		lazy = true,
+		config= function ()
+			vim.notify = require("notify")
+		end
 	},
 }
