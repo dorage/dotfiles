fe66caf35812e7c056c6847d9b0a9aeb8e109c67
dorage-san <baloonflower554@gmail.com>
2024-04-16 12:50:54 +0900

    remove: dressing

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -155,29 +155,12 @@ return {
 			})
 		end,
 	},
-	-- pretty vim select/input hook
-	{
-		"stevearc/dressing.nvim",
-		lazy = true,
-		init = function()
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.select = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.select(...)
-			end
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.input = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.input(...)
-			end
-		end,
-	},
 	-- notify
 	{
 		"rcarriga/nvim-notify",
 		lazy = true,
-		config= function ()
+		config = function()
 			vim.notify = require("notify")
-		end
+		end,
 	},
 }
