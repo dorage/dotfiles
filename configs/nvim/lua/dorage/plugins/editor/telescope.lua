c82493d55dbe98ab40ef0cee773ca80915487091
dorage-san <baloonflower554@gmail.com>
2024-04-19 20:31:41 +0900

    fix: markdown&telekasten keymaps

--- configs/nvim/lua/dorage/plugins/editor/telescope.lua
+++ configs/nvim/lua/dorage/plugins/editor/telescope.lua
@@ -71,9 +71,9 @@ return {
 			vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
 			vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
 			vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-			vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
+			vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_templated_note<CR>")
 			vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-			vim.keymap.set("n", "<leader>zl", "<cmd>Telekasten insert_link<CR>")
+			vim.keymap.set("n", "<leader>zi", "<cmd>Telekasten insert_link<CR>")
 		end,
 	},
 	{
