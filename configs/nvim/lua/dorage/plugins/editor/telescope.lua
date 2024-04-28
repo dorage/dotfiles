6d225a891431d3b48e2ab38261f076f1137a9fa4
dorage <baloonflower554@gmail.com>
2024-04-28 11:33:11 +0900

    fix: telekasten location

--- configs/nvim/lua/dorage/plugins/editor/telescope.lua
+++ configs/nvim/lua/dorage/plugins/editor/telescope.lua
@@ -61,7 +61,7 @@ return {
 		dependencies = { "nvim-telescope/telescope.nvim" },
 		config = function()
 			require("telekasten").setup({
-				home = vim.fn.expand("~/workspace/thinking-vault/vaults"),
+				home = vim.fn.expand("~/.config/dorage-vault"),
 			})
 
 			-- Launch panel if nothing is typed after <leader>z
