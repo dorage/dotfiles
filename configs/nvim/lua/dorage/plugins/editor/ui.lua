a93b76573f0e76f2a58f82164b0ae05e455282d3
dorage-san <baloonflower554@gmail.com>
2024-05-02 14:50:28 +0900

    add: which-key border

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -128,9 +128,9 @@ return {
 			vim.o.timeoutlen = 300
 		end,
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			window = {
+				border = "single",
+			},
 		},
 	},
 	-- cursor movement
