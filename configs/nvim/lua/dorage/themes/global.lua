fe843089f0ee15e2a788752728ba77fca2b7fe90
dorage-san <baloonflower554@gmail.com>
2024-05-02 14:50:52 +0900

    chore: fix comment

--- configs/nvim/lua/dorage/themes/global.lua
+++ configs/nvim/lua/dorage/themes/global.lua
@@ -2,7 +2,7 @@ local afterThemeConfig = function()
 	-- setup transparent bg
 	vim.call("background#enable")
 
-	-- setup line number
+	-- setup line number highlighting
 	vim.api.nvim_set_hl(0, "lineNr", { fg = "#16FF00", bold = true })
 	vim.api.nvim_set_hl(0, "lineNrAbove", { fg = "#AAAAAA", bold = true })
 	vim.api.nvim_set_hl(0, "lineNrBelow", { fg = "#AAAAAA", bold = true })
