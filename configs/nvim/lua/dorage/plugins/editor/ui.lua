096e90ad8e0ad5f4aec7d6a2174866cb421fa8dd
dorage-san <baloonflower554@gmail.com>
2024-05-02 15:11:26 +0900

    add: which-key icon

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -131,6 +131,11 @@ return {
 			window = {
 				border = "single",
 			},
+			icons = {
+				breadcrumb = "󰇘", -- symbol used in the command line area that shows your active key combo
+				separator = "", -- symbol used between a key and it's label
+				group = " ", -- symbol prepended to a group
+			},
 		},
 	},
 	-- cursor movement
