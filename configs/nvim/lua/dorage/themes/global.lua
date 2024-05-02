fa986d0fde2d4d548f26fc1fde6a67ec193c7a15
dorage-san <baloonflower554@gmail.com>
2024-05-02 15:04:12 +0900

    add: clear highlight & line number hl

--- configs/nvim/lua/dorage/themes/global.lua
+++ configs/nvim/lua/dorage/themes/global.lua
@@ -4,8 +4,13 @@ local afterThemeConfig = function()
 
 	-- setup line number highlighting
 	vim.api.nvim_set_hl(0, "lineNr", { fg = "#16FF00", bold = true })
-	vim.api.nvim_set_hl(0, "lineNrAbove", { fg = "#AAAAAA", bold = true })
-	vim.api.nvim_set_hl(0, "lineNrBelow", { fg = "#AAAAAA", bold = true })
+	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#16FF00", bold = true })
+	vim.api.nvim_set_hl(0, "lineNrAbove", { fg = "#AAAAAA", bold = false })
+	vim.api.nvim_set_hl(0, "lineNrBelow", { fg = "#AAAAAA", bold = false })
+
+	-- clear float highlight
+	vim.api.nvim_set_hl(0, "NormalFloat", { guibg = nil })
+	vim.api.nvim_set_hl(0, "FloatBorder", { guibg = nil })
 
 	-- setup current line highlight
 	vim.opt.cursorline = true
