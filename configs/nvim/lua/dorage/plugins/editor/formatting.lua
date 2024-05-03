27e3391f4946f9f9238cae40e9d7cc0d37f0946b
dorage-san <baloonflower554@gmail.com>
2024-05-03 23:46:09 +0900

    add: perl formatter

--- configs/nvim/lua/dorage/plugins/editor/formatting.lua
+++ configs/nvim/lua/dorage/plugins/editor/formatting.lua
@@ -31,6 +31,7 @@ return {
 				json = { "prettier" },
 				astro = { "prettier" },
 				yaml = { "prettier" },
+				perl = { "perltidy" },
 			},
 			-- Set up format-on-save
 			format_on_save = { timeout_ms = 500, lsp_fallback = true, async = true },
