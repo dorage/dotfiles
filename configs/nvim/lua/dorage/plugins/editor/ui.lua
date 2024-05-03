7b4460afdad979cf8f32352f7e82fae105a1b1b0
dorage-san <baloonflower554@gmail.com>
2024-05-03 19:34:14 +0900

    add: cursor icon

--- configs/nvim/lua/dorage/plugins/editor/ui.lua
+++ configs/nvim/lua/dorage/plugins/editor/ui.lua
@@ -145,8 +145,7 @@ return {
 			require("smoothcursor").setup({
 				type = "default", -- Cursor movement calculation method, choose "default", "exp" (exponential) or "matrix".
 
-				-- cursor = "🍕", -- Cursor shape (requires Nerd Font). Disabled in fancy modee.
-				cursor = "", -- Cursor shape (requires Nerd Font). Disabled in fancy modee.
+				cursor = "", -- Cursor shape (requires Nerd Font). Disabled in fancy modee.
 				texthl = "SmoothCursor", -- Highlight group. Default is { bg = nil, fg = "#FFD400" }. Disabled in fancy mode.
 				linehl = nil, -- Highlights the line under the cursor, similar to 'cursorline'. "CursorLine" is recommended. Disabled in fancy mode.
 
