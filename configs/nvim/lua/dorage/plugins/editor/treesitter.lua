ebebb4e189bb51782f10ef330e423ec95ed232c7
dorage-san <baloonflower554@gmail.com>
2024-05-04 17:53:50 +0900

    add: treesitter-context

--- configs/nvim/lua/dorage/plugins/editor/treesitter.lua
+++ configs/nvim/lua/dorage/plugins/editor/treesitter.lua
@@ -133,10 +133,33 @@ return {
 			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
 			-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+			-- It makes every textobject to movement like dtf makes tf
+			-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+			-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+			-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+			-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+				line_numbers = true,
+				multiline_threshold = 10, -- Maximum number of lines to show for a single context
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = "⎯",
+				zindex = 20, -- The Z-index of the context window
+				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+			})
+
+			vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { bg = nil })
+			vim.api.nvim_set_hl(0, "TreesitterContext", { bg = nil })
 		end,
 	},
 }
