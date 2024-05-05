405229f67186ff9e566283789d8998867850157a
dorage-san <baloonflower554@gmail.com>
2024-05-05 12:53:50 +0900

    add: keymap, nodejs setup

--- configs/nvim/lua/dorage/plugins/editor/debugging.lua
+++ configs/nvim/lua/dorage/plugins/editor/debugging.lua
@@ -1,17 +1,163 @@
 return {
-	{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
 	{
 		"mfussenegger/nvim-dap",
-		depends = { "rcarriga/nvim-dap-ui" },
+		dependencies = {
+			{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {},
+			},
+			-- node.js setups
+			"mxsdev/nvim-dap-vscode-js",
+			{
+				"williamboman/mason.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					table.insert(opts.ensure_installed, "js-debug-adapter")
+				end,
+			},
+			{
+				"microsoft/vscode-js-debug",
+				-- it might need to install setuptools
+				-- 'setuptools' is removed in python 3.12
+				-- https://stackoverflow.com/questions/69919970/no-module-named-distutils-but-distutils-installed
+				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+			},
+		},
+		-- lazyvim setup
+		keys = {
+			{
+				"<leader>dB",
+				function()
+					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+				end,
+				desc = "Breakpoint Condition",
+			},
+			{
+				"<leader>db",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "Toggle Breakpoint",
+			},
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+				desc = "Continue",
+			},
+			-- {
+			-- 	"<leader>da",
+			-- 	function()
+			-- 		require("dap").continue({ before = get_args })
+			-- 	end,
+			-- 	desc = "Run with Args",
+			-- },
+			{
+				"<leader>dC",
+				function()
+					require("dap").run_to_cursor()
+				end,
+				desc = "Run to Cursor",
+			},
+			{
+				"<leader>dg",
+				function()
+					require("dap").goto_()
+				end,
+				desc = "Go to Line (No Execute)",
+			},
+			{
+				"<leader>di",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "Step Into",
+			},
+			{
+				"<leader>dj",
+				function()
+					require("dap").down()
+				end,
+				desc = "Down",
+			},
+			{
+				"<leader>dk",
+				function()
+					require("dap").up()
+				end,
+				desc = "Up",
+			},
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "Run Last",
+			},
+			{
+				"<leader>do",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "Step Out",
+			},
+			{
+				"<leader>dO",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Step Over",
+			},
+			{
+				"<leader>dp",
+				function()
+					require("dap").pause()
+				end,
+				desc = "Pause",
+			},
+			{
+				"<leader>dr",
+				function()
+					require("dap").repl.toggle()
+				end,
+				desc = "Toggle REPL",
+			},
+			{
+				"<leader>ds",
+				function()
+					require("dap").session()
+				end,
+				desc = "Session",
+			},
+			{
+				"<leader>dt",
+				function()
+					require("dap").terminate()
+				end,
+				desc = "Terminate",
+			},
+			{
+				"<leader>dw",
+				function()
+					require("dap.ui.widgets").hover()
+				end,
+				desc = "Widgets",
+			},
+		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
+			dapui.setup({})
 
-			-- dap ui
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
 			end
 			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.after.event_initialized.dapui_config = function()
 				dapui.open()
 			end
 			dap.listeners.before.event_terminated.dapui_config = function()
@@ -22,8 +168,12 @@ return {
 			end
 
 			-- dap keymap
-			vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Debug:Toggle breakpoint" })
-			vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debug:Continue" })
+			vim.keymap.set("n", "<leader>fd", function()
+				dapui.toggle({})
+			end, { desc = "Debug:Toggle UI" })
+			vim.keymap.set({ "n", "v" }, "<leader>de", function()
+				dapui.eval()
+			end, { desc = "Debug:Eval" })
 
 			-- load debugger configs
 			require("dorage.configs.debuggers")
