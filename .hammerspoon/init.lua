2c60cde2cc08f86b460d32340fdc8f5b3de4f4ea
dorage-san <baloonflower554@gmail.com>
2024-12-03 13:49:07 +0900

    add: hammerspoon annotation for lua_ls

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,7 +4,12 @@ require("hs.ipc")
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
 	hs.reload()
 end)
-hs.alert.show("Config loaded")
+
+-- generate hammerspoon annotation
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "L", function()
+	require("Spoons.Annotation").init()
+	hs.alert.show("HS annotation generated")
+end)
 
 -- import modules
 require("Spoons.language")
@@ -12,3 +17,5 @@ require("Spoons.hotkey-window")
 require("Spoons.tiling")
 
 require("sketchybar")
+
+hs.alert.show("Config loaded")
