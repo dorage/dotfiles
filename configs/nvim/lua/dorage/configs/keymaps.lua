fbb4e77da40e1e1f714a2a18fef4599720e396de
dorage-san <baloonflower554@gmail.com>
2025-01-06 15:50:25 +0900

    add move to last used buffer

--- configs/nvim/lua/dorage/configs/keymaps.lua
+++ configs/nvim/lua/dorage/configs/keymaps.lua
@@ -58,6 +58,7 @@ vim.keymap.set({ "n" }, "<c-d>", "<c-d>zz", { silent = true })
 vim.keymap.set({ "n" }, "<a-c>", "<cmd>bp<cr><esc>", { desc = "delete current buffer" })
 vim.keymap.set({ "n" }, "<a-.>", "<cmd>bn<cr><esc>", { desc = "move to next buffer" })
 vim.keymap.set({ "n" }, "<a-,>", "<cmd>bp<cr><esc>", { desc = "move to prev buffer" })
+vim.keymap.set({ "n" }, "<a-;>", "<cmd>b#<cr><esc>", { desc = "move to last used buffer" })
 
 -- httpyac
 vim.keymap.set({ "n" }, "<leader>hh", function()
