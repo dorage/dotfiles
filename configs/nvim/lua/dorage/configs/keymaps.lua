26b1b1e549fcec1d05b3f0fbdcb907fd77be15b0
dorage-san <baloonflower554@gmail.com>
2025-01-06 16:03:30 +0900

    remove duplicate keymap, unused keymaps

--- configs/nvim/lua/dorage/configs/keymaps.lua
+++ configs/nvim/lua/dorage/configs/keymaps.lua
@@ -5,22 +5,21 @@ vim.keymap.set(
 	"<cmd>lua vim.diagnostic.open_float()<cr><esc>",
 	{ desc = "Show diagnostic in a floating window" }
 )
-vim.keymap.set(
-	{ "n" },
-	"[d",
-	"<cmd>lua vim.diagnostic.goto_prev()<cr><esc>",
-	{ desc = "Move to the previous diagnostic in the current buffer" }
-)
-vim.keymap.set(
-	{ "n" },
-	"]d",
-	"<cmd>lua vim.diagnostic.goto_next()<cr><esc>",
-	{ desc = "Move to the next diagnostic in the current buffer" }
-)
+-- vim.keymap.set(
+-- 	{ "n" },
+-- 	"[d",
+-- 	"<cmd>lua vim.diagnostic.goto_prev()<cr><esc>",
+-- 	{ desc = "Move to the previous diagnostic in the current buffer" }
+-- )
+-- vim.keymap.set(
+-- 	{ "n" },
+-- 	"]d",
+-- 	"<cmd>lua vim.diagnostic.goto_next()<cr><esc>",
+-- 	{ desc = "Move to the next diagnostic in the current buffer" }
+-- )
 
 -- blazing fast save
 vim.keymap.set({ "i", "x", "n", "s" }, "<leader>w", "<cmd>:w<cr>", { desc = "Save file" })
-vim.keymap.set({ "n", "s" }, "<leader>n", "<cmd>noh<cr><esc>", { desc = ":noh" })
 
 -- yank to clipboard
 vim.keymap.set({ "n", "s" }, "<leader>y", '"+y', { desc = "Copy to clipboard" })
@@ -50,15 +49,15 @@ vim.keymap.set({ "n" }, "<space>sh", "<c-w>s", { desc = "Split window horizontal
 vim.keymap.set({ "n" }, "<space>sv", "<c-w>v", { desc = "Split window vertically" })
 
 -- etc
-vim.keymap.set({ "n", "s" }, "<leader>n", "<cmd>noh<cr><esc>", { desc = ":noh" })
-vim.keymap.set({ "n" }, "<c-u>", "<c-u>zz", { silent = true })
-vim.keymap.set({ "n" }, "<c-d>", "<c-d>zz", { silent = true })
+vim.keymap.set({ "n", "s" }, "<leader>n", "<cmd>noh<cr><esc>", { desc = "No highlight" })
+vim.keymap.set({ "n" }, "<c-u>", "<c-u>zz", { silent = true, desc = "scroll up and center" })
+vim.keymap.set({ "n" }, "<c-d>", "<c-d>zz", { silent = true, desc = "scroll down and center" })
 
 -- buffer manipulation
 vim.keymap.set({ "n" }, "<a-c>", "<cmd>bp<cr><esc>", { desc = "delete current buffer" })
 vim.keymap.set({ "n" }, "<a-.>", "<cmd>bn<cr><esc>", { desc = "move to next buffer" })
 vim.keymap.set({ "n" }, "<a-,>", "<cmd>bp<cr><esc>", { desc = "move to prev buffer" })
-vim.keymap.set({ "n" }, "<a-;>", "<cmd>b#<cr><esc>", { desc = "move to last used buffer" })
+vim.keymap.set({ "n" }, "<a-;>", "<cmd>b#<cr><esc>", { desc = "move to last buffer" })
 
 -- httpyac
 vim.keymap.set({ "n" }, "<leader>hh", function()
