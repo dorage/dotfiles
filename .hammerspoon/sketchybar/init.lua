54042629185713346415c15246c20b109c7389c9
dorage-san <baloonflower554@gmail.com>
2025-03-05 15:44:25 +0900

    remove widgets

--- .hammerspoon/sketchybar/init.lua
+++ .hammerspoon/sketchybar/init.lua
@@ -1,7 +1,7 @@
 local manager = require("sketchybar.manager")
 
 manager.add_widget(require("sketchybar.widgets.active-app"), { only_init = true })
-manager.add_widget(require("sketchybar.widgets.battery"), { time = 30 })
-manager.add_widget(require("sketchybar.widgets.memory"), { time = 10 })
-manager.add_widget(require("sketchybar.widgets.storage"), { time = 30 })
+-- manager.add_widget(require("sketchybar.widgets.battery"), { time = 120 })
+-- manager.add_widget(require("sketchybar.widgets.memory"), { time = 120 })
+-- manager.add_widget(require("sketchybar.widgets.storage"), { time = 120 })
 manager.add_widget(require("sketchybar.widgets.wifi"), { only_init = true })
