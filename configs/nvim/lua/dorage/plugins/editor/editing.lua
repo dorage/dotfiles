405a963f372f626aa86fd89e86f37ef3739bb512
dorage-san <baloonflower554@gmail.com>
2025-03-20 01:24:51 +0900

    remove: spector

--- configs/nvim/lua/dorage/plugins/editor/editing.lua
+++ configs/nvim/lua/dorage/plugins/editor/editing.lua
@@ -89,21 +89,4 @@ return {
 			)
 		end,
 	},
-	{
-		"nvim-pack/nvim-spectre",
-		config = function()
-			vim.keymap.set("n", "<leader>so", '<cmd>lua require("spectre").toggle()<CR>', {
-				desc = "Toggle Spectre",
-			})
-			vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-				desc = "Search current word",
-			})
-			vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-				desc = "Search current word",
-			})
-			vim.keymap.set("n", "<leader>ep", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-				desc = "Search on current file",
-			})
-		end,
-	},
 }
