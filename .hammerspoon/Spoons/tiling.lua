0c576b25f4b3f71f22e7cf60713320e7b9ce6028
dorage-san <baloonflower554@gmail.com>
2025-03-31 00:54:04 +0900

    add window chooser & bind key for choosing a windwo

--- .hammerspoon/Spoons/tiling.lua
+++ .hammerspoon/Spoons/tiling.lua
@@ -47,6 +47,28 @@ hs.hotkey.bind({ "cmd", "shift" }, "h", function()
 	local activeWindow = hs.window.focusedWindow()
 	hs.grid.set(activeWindow, "1,0 7x2")
 end)
+
+local windowChooser = function(active_win_id, callback)
+	local chooser = hs.chooser.new(callback)
+	local windows = hs.window.allWindows()
+
+	local win_list
+	-- remove active window
+	win_list = hs.fnutils.filter(windows, function(win)
+		return win:id() ~= active_win_id
+	end)
+	-- format for chooser
+	win_list = hs.fnutils.map(win_list, function(win)
+		return {
+			text = win:title(),
+			subText = win:application():name(),
+			image = hs.image.imageFromAppBundle(win:application():bundleID()),
+			value = win:id(),
+		}
+	end)
+	chooser:choices(win_list)
+	chooser:show()
+end
 -- full
 hs.hotkey.bind({ "cmd", "shift" }, "k", function()
 	local activeWindow = hs.window.focusedWindow()
@@ -57,8 +79,11 @@ hs.hotkey.bind({ "cmd", "shift" }, "l", function()
 	local activeWindow = hs.window.focusedWindow()
 	hs.grid.set(activeWindow, "8,0 7x2")
 end)
--- fullscreen
+
+-- find & focus
 hs.hotkey.bind({ "cmd", "shift" }, "f", function()
 	local activeWindow = hs.window.focusedWindow()
-	hs.grid.set(activeWindow, "0,0 15x2")
+	windowChooser(activeWindow:id(), function(choice)
+		hs.window(choice.value):focus()
+	end)
 end)
