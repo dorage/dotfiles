d0052aaf505828ff43d2ffc7cf15d34ad681b197
dorage-san <baloonflower554@gmail.com>
2025-06-02 21:49:23 +0900

    -

--- configs/nvim/lua/dorage/configs/keymaps.lua
+++ configs/nvim/lua/dorage/configs/keymaps.lua
@@ -80,36 +80,3 @@ vim.keymap.set({ "n" }, "<leader>ao", function()
 		"zellij run --floating --name lazygit --width 90% --height 90% -x 5% -y 5% --close-on-exit -- lazydocker"
 	)
 end, { desc = "open lazydocker" })
-
--- treesitter test
-vim.keymap.set({ "n" }, "<leader>oa", function()
-	local lang = vim.bo.filetype
-	local parser = vim.treesitter.get_parser(0, lang)
-
-	if not parser then
-		print("Treesitter parser not found for " .. lang)
-		return
-	end
-
-	local tree = parser:parse()[1]
-	-- local query_string = "(function_declaration name: (identifier) @function.name)"
-	local query_string = "(export_statement)"
-	local query = vim.treesitter.query.parse(lang, query_string)
-
-	if query then
-		print("Functions found:")
-		-- 트리의 루트 노드에서 쿼리 실행 및 캡처 반복
-		-- iter_captures는 (capture_id, node, metadata) 형태로 결과를 반환
-		for id, node, metadata in
-			query:iter_captures(tree:root(), 0, vim.api.nvim_win_get_cursor(0)[1], vim.api.nvim_buf_line_count(0))
-		do
-			local name = query.captures[id]
-
-			local type = node:type() -- type of the captured node
-			local row1, col1, row2, col2 = node:range() -- range of the capture
-			print(name, type, vim.inspect(vim.api.nvim_buf_get_text(0, row1, col1, row2, col2, {})))
-		end
-	else
-		print("Failed to parse query string.")
-	end
-end, { desc = "Httpyac: reqeust current buffer" })
