996f5c49db0bd938ab3fa423a34ae62612a46e29
kanghyunlee <kanghyunlee@kanghyunleeui-MacBookAir.local>
2025-08-06 09:58:36 +0900

    add github shortcut: ghib

--- configs/scripts/git/github.sh
+++ configs/scripts/git/github.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:$PATH"
 
 # Commit with Gitmoji
 alias gcg="~/.config/scripts/git/gitmoji-commit.sh"
@@ -8,6 +7,12 @@ alias gcg="~/.config/scripts/git/gitmoji-commit.sh"
 # Checkout branch with fzf
 function gco() {
 	git checkout $(git branch | fzf)
+}
+
+# Issue - create branch from issue, then checkout
+function ghib() {
+	# get issue id
+	gh issue develop $( gh issue list | fzf | sed 's/\([0-9]*\).*/\1/' ) --base $( git branch -r | fzf | sed 's/origin\///' )  --checkout
 }
 
 # PR - create
