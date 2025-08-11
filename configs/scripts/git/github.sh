2666b20b2a14bb852fdf00b4c3dae4e887d5dba3
kanghyunlee <kanghyunlee@kanghyunleeui-MacBookAir.local>
2025-08-12 08:36:37 +0900

    github shortcut: ghic, ghib

--- configs/scripts/git/github.sh
+++ configs/scripts/git/github.sh
@@ -9,15 +9,25 @@ function gco() {
 	git checkout $(git branch | fzf)
 }
 
+# Issue - create
+function ghic() {
+	gh issue create
+}
+ 
 # Issue - create branch from issue, then checkout
 function ghib() {
 	# get issue id
-	gh issue develop $( gh issue list | fzf | sed 's/\([0-9]*\).*/\1/' ) --base $( git branch -r | fzf | sed 's/origin\///' )  --checkout
+	gh issue develop $(gh issue list --state open | fzf | sed 's/\([0-9]*\).*/\1/') --base $( git branch -r | fzf | sed 's/origin\///' )  --checkout
 }
 
 # PR - create
 function ghpr() {
 	gh pr create --fill --base $(git branch -r | fzf | sed 's/origin\///')
+}
+
+# PR - edit
+function ghpre() {
+	gh pr edit $(gh pr list | fzf | sed 's/\([0-9]*\).*/\1/')
 }
 
 # PR - merge with squash
