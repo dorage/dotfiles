ecab019bba4215a9643841b6bc11519d147de07d
kanghyunlee <kanghyunlee@kanghyunleeui-MacBookAir.local>
2025-09-12 10:44:58 +0900

    깃헙 단축: 자동 닫기, 이슈 id가져오기 등

--- configs/scripts/git/github.sh
+++ configs/scripts/git/github.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+function current_branch_issue_id() {
+	git branch --show-current | sed 's/^\([0-9]*\).*/\1/'
+}
 
 # Commit with Gitmoji
 alias gcg="~/.config/scripts/git/gitmoji-commit.sh"
@@ -9,6 +12,17 @@ function gco() {
 	git checkout $(git branch | fzf)
 }
 
+
+# commit closing issue id
+function gcr() {
+	issue_id=$(current_branch_issue_id)
+	if [ -n "$issue_id" ]; then
+		git commit --allow-empty -m "Close #$issue_id"
+	else
+		echo "No issue ID found in current branch name"
+	fi
+}
+
 # Issue - create
 function ghic() {
 	gh issue create
@@ -16,13 +30,20 @@ function ghic() {
  
 # Issue - create branch from issue, then checkout
 function ghib() {
-	# get issue id
-	gh issue develop $(gh issue list --state open | fzf | sed 's/\([0-9]*\).*/\1/') --base $( git branch -r | fzf | sed 's/origin\///' )  --checkout
+	gh issue develop $(gh issue list --state open | fzf | sed 's/\([0-9]*\).*/\1/') --base $( git branch --remotes | fzf | sed 's/origin\///' )  --checkout
 }
 
 # PR - create
 function ghpr() {
-	gh pr create --fill --base $(git branch -r | fzf | sed 's/origin\///')
+	gcr
+	git push
+	# If current branch is created by `gh issue develop --base`, then base branch will be configured as the base for pr
+	gh pr create --fill
+}
+
+# PR - create
+function ghprc() {
+	gh pr create --fill --base $(git branch --remotes | fzf | sed 's/origin\///')
 }
 
 # PR - edit
@@ -36,3 +57,16 @@ function ghprm() {
 }
 
 
+# PR - create & squash merge automatically
+function ghprma() {
+	gcr
+	git push
+	# If current branch is created by `gh issue develop --base`, then base branch will be configured as the base for pr
+	gh pr create --fill
+	gh pr merge --squash --auto
+}
+
+# Worktree - add git worktree with branch
+function gwta() {
+	branch=$(git branch -r | fzf) && git worktree add "./worktrees/$branch" $branch
+}
